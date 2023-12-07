08b31d7bcf500daa09ff044008b67cdd944eb8d6
axcheiste-rooney-mara <153155442+axcheiste-rooney-mara@users.noreply.github.com>
2023-12-06 22:53:48 -0600

    A Randolf Christmas ""Comprehensive""
    A simple program to sort you're readme files...

---
+++ untitled/src/Main.java
@@ -0,0 +1,44 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        int Readme = 225560;
+        int R = 120;
+
+        for (Readme = Readme; R > 0; R++) {
+
+            int list = 120;
+            int s_list = 0;
+
+            s_list++;
+
+        }
+            while (s_list > list) {
+
+                Readme++;
+
+                System.in.out.println(" Home and welcome! " + Readme);
+                System.in.out.println(s_list + Readme);
+                System.in.println(list);
+
+
+            }
+
+        }
+        System.in.println(" Hello and welcome" + "120 Account Primitives " + R);
+
+
+        int yasm = 800;
+        String yasm_r = "1200 Accounts Primitives " + 1;
+
+        for (int i = 1; i <= 5; i++) {
+            yasm++;
+            yasm_r++;
+
+
+            System.in.out.println(" Hello and welcome" + "120 Account Primitives ");
+            System.out.println(i + " = 1200i - ysmr ");
+        }
+    }
+}
